d3cf3b3706ee4a65f338d2afea2714b63e345797
Firos sha muhammad <96859506+firossha321@users.noreply.github.com>
2023-12-18 09:37:43 +0530

    query.py

--- plugins/query.py
+++ plugins/query.py
@@ -470,7 +470,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', url='https://github.com/firossha321/PROFESSOR-BOT')
+            InlineKeyboardButton('ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', url='https://github.com/firossha321/Movie_bot')
             ],[
             InlineKeyboardButton('‹ Bᴀᴄᴋ', 'about')
         ]]
