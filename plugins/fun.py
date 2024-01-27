e951d9dc9bb1349a788ef29a2bb691ba018d7a1f
firoshamhd <135963904+firoshamhd@users.noreply.github.com>
2024-01-27 19:33:38 +0530

    Update fun.py

--- plugins/fun.py
+++ plugins/fun.py
@@ -6,7 +6,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
-from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, DISE, PICS, PICS2, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, START_MESSAGE, FORCE_SUB_TEXT, SUPPORT_CHAT
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, TOSS, LOG_CHANNEL, DISE, PICS, PICS2, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, START_MESSAGE, FORCE_SUB_TEXT, SUPPORT_CHAT
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
 from database.connections_mdb import active_connection
 
@@ -17,3 +17,8 @@ BATCH_FILES = {}
 async def dise(client, message):
     cap="ᴛʜɪꜱ ɪꜱ ʏᴏᴜʀ ꜱᴄᴏʀᴇ"
     await message.reply_photo(photo=random.choice(DISE),caption=cap)
+
+@Client.on_message(filters.command("toss")) 
+async def toss(client, message):
+    cap1="ᴛʜɪꜱ ɪꜱ ʏᴏᴜʀ ʀᴇꜱᴜʟᴛ"
+    await message.reply_photo(photo=random.choice(TOSS),caption=cap1)
