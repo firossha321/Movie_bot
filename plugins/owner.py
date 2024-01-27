53d8083e5e2397268aba9f930a34d7876ad6b26c
firoshamhd <135963904+firoshamhd@users.noreply.github.com>
2024-01-27 10:34:34 +0530

    Update owner.py

--- plugins/owner.py
+++ plugins/owner.py
@@ -3,7 +3,7 @@ from pyrogram.errors import ChatAdminRequired
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid, UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 
-from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, WELCOM_PIC, WELCOM_TEXT, IMDB_TEMPLATE
+from info import PICS2, ADMINS, LOG_CHANNEL, SUPPORT_CHAT, WELCOM_PIC, WELCOM_TEXT, IMDB_TEMPLATE
 from utils import get_size, temp, extract_user, get_file_id, get_poster, humanbytes
 from database.users_chats_db import db
 from database.ia_filterdb import Media
@@ -14,7 +14,7 @@ import logging, re, asyncio, time, shutil, psutil, os, sys
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-@Client.on_message(filters.command('owner') & filters.incoming)
-async def get_ownr(bot, message):
-    own = await message.reply(script.PROGRAMMER_TXT)
-    await rju.edit(InputMediaPhoto(random.choice(PICS2),script.PROGRAMMER_TXT))
+@Client.on_message(filters.command("owner")) 
+async def image(client, message):
+    await message.reply_photo(photo=random.choice(PICS2),caption=PROGRAMMER_TXT) 
+
