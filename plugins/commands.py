6fb3487f80e17caecb3a65f631f4630646186984
firoshamhd <135963904+firoshamhd@users.noreply.github.com>
2024-01-27 14:07:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,7 +6,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
-from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, DISE, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, START_MESSAGE, FORCE_SUB_TEXT, SUPPORT_CHAT
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, DISE, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, START_MESSAGE, FORCE_SUB_TEXT, SUPPORT_CHAT, PROGRAMMER_TXT
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
 from database.connections_mdb import active_connection
 
@@ -396,5 +396,7 @@ async def geg_template(client, message):
 async def image(client, message):
     await message.reply_photo(photo=random.choice(DISE)) 
 
-
+@Client.on_message(filters.command("owner")) 
+async def image(client, message):
+    await message.reply_photo(photo=random.choice(PICS2),caption=script.PROGRAMMER_TXT) 
 
