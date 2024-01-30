97b6ff4b4ce4190b323e3641ad7a4eab946be2c4
firoshamhd <135963904+firoshamhd@users.noreply.github.com>
2024-01-30 17:20:19 +0530

    Update fun.py

--- plugins/fun.py
+++ plugins/fun.py
@@ -1,6 +1,7 @@
 import os, re, json, base64, logging, random, asyncio
 
 from Script import script
+import time
 from database.users_chats_db import db
 from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
@@ -13,12 +14,33 @@ from database.connections_mdb import active_connection
 logger = logging.getLogger(__name__)
 BATCH_FILES = {}
 
-@Client.on_message(filters.command("dise")) 
-async def dise(client, message):
-    cap="ᴛʜɪꜱ ɪꜱ ʏᴏᴜʀ ꜱᴄᴏʀᴇ"
-    await message.reply_photo(photo=random.choice(DISE),caption=cap)
-
 @Client.on_message(filters.command("toss")) 
 async def toss(client, message):
     cap1="ᴛʜɪꜱ ɪꜱ ʏᴏᴜʀ ʀᴇꜱᴜʟᴛ"
+    firos = await message.reply_sticker("CAACAgIAAxkBAAIVrmW44AABninrQsje8r0Nvl-EoJPn0AACvgkAAoSumEoSQwToTbAzsh4E") 
+    time.sleep(3)
     await message.reply_photo(photo=random.choice(TOSS),caption=cap1)
+    await firos.delete()
+
+DICE_E_MOJI = "🎲"
+# EMOJI CONSTANTS
+
+
+@Client.on_message(
+    filters.command(["roll", "dice"], COMMAND_HAND_LER) &
+    f_onw_fliter
+)
+async def roll_dice(client, message):
+    rep_mesg_id = message.id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.id
+    r = await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=DICE_E_MOJI,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
+    time.sleep(3)
+    cap="ᴛʜɪꜱ ɪꜱ ʏᴏᴜʀ ꜱᴄᴏʀᴇ"
+    await message.reply_photo(photo=random.choice(DISE),caption=cap)
+    await r.delete()
