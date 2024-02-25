dd2ed963d8e05370ba85336244a460560a967581
Firos sha muhammad <96859506+firossha321@users.noreply.github.com>
2024-02-25 13:45:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -209,7 +209,7 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
         await abc.delete()        
     if pmspoll:
         await msg.message.delete()
-
+        await firo.delete()
 
 async def pm_spoll_choker(msg):
     query = re.sub(r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)", "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
